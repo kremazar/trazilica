4b133e22a9397e743fdeac0d9938f05a64cc6407
kremazar <kmazar222@gmail.com>
2020-05-23 16:52:28 +0200

    init

---
+++ src/test/trazilica.spec.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+var chai = require("chai");
+var expect = chai.expect;
+var should = chai.should;
+const trazilica = require("../trazilica");
+
+describe.skip('trazilica', function() {
+    it('trazilica should be a function', function () {
+        trazilica();
+    });
+  
+});
+
+
+
+
+
+ 
