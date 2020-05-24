38bd7f1ba8f145adefb75b7e89fe61b7c3c38c5f
kremazar <kmazar222@gmail.com>
2020-05-25 00:57:34 +0200

    red
    red

--- src/test/trazilica.spec.js
+++ src/test/trazilica.spec.js
@@ -4,11 +4,14 @@ var expect = chai.expect;
 var should = chai.should;
 const trazilica = require("../trazilica");
 
-describe.skip('trazilica', function() {
+describe('trazilica', function() {
     it('trazilica should be a function', function () {
         trazilica();
     });
-  
+    it('I return 3 suggestion', function () {
+        let result = trazilica("I")
+        assert.deepEqual(result,['I have decided', 'I will try', 'I will just'])
+    });
 });
 
 
---
+++ src/trazilica.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+
+function suggestionTool(currentText){
+    
+
+    
+}
+console.log(suggestionTool("I"))
+
+module.exports = suggestionTool;
