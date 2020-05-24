d6f8bb57d8f581dbe97ca9721feebe8afb97a700
kremazar <kmazar222@gmail.com>
2020-05-25 00:58:03 +0200

    green
    green

--- src/trazilica.js
+++ src/trazilica.js
@@ -1,7 +1,23 @@
 var fs = require('fs');
 
 function suggestionTool(currentText){
+    var text = fs.readFileSync('long.txt','utf8')
+    let sve = []
+    sve=text
+    var formatirano = sve.split(" ")
     
+    result = formatirano.reduce(function(a, e, i) {
+        if (e === currentText)
+            a.push(i);
+        return a;
+    }, []);
+    let rezultati= []
+    for (i=0;i<3;i++){
+        
+        rezultati.push(formatirano[result[i]] + " " +  formatirano[result[i]+1] + " " + formatirano[result[i]+2])
+    }
+    
+    return rezultati 
 
     
 }
